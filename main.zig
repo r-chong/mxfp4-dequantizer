d2afbe729fbf8ec3a758b451716e57e3e4ba84a0
r-chong <dev.reese.chong@gmail.com>
2025-12-01 23:11:29 -0500

    create blueprint of main file

--- main.zig
+++ main.zig
@@ -1,7 +1,17 @@
 const std = @import("std");
 
-// fmt is format string
-// args is tuple of placeholders. see {s}
-pub fn main() void {
-    std.debug.print("Hello {s}!\n", .{"World"});
-}
+// 8 bytes is the size of a safetensors json header (u64)
+const HEADER_SIZE_BUF_SIZE = 8;
+const SAFETENSORS_PATH = "./gpt-oss-20b/original/model.safetensors";
+
+// LayerMetadata struct
+
+// layers_info
+
+// function get_safetensors_content - get header + raw tensor data (weights)
+
+// function block_decoder - process individual block
+
+// function tensor_split - turn tensor into individual blocks
+
+// function select tensor - how are we choosing the tensor to start with
